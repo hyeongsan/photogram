ac3ad5446ed507a31f168ab2d4f482c6c936db5d
hyeongsan <77267916+hyeongsan@users.noreply.github.com>
2022-08-12 15:18:54 +0900

    Delete photogram directory

--- photogram/src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cos.photogramstart.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//JpaRepository를 상속했으면 어노테이션이 없어도 Ioc등록이 자동으로된다.
-public interface UserRepository extends JpaRepository<User,Integer> {
-	// JPA query method
-	User findByUsername(String username);
-}

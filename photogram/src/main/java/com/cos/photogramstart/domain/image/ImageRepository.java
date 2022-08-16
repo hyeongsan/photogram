0f5e47909f32db6fe97b4eeec5e1a12050cae7ce
hyeongsan <sanstory12rt@naver.com>
2022-08-16 21:16:29 +0900

    0816 인기페이지 구현완료

--- photogram/src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
+++ photogram/src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.domain.image;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +9,11 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ImageRepository extends JpaRepository<Image,Integer> {
 	
-	@Query(value="select * from image where userId IN (select toUserId from subscribe where fromUserId= :principalId) ORDER BY id DESC", nativeQuery=true)
+	@Query(value="select * from image where userId IN (select toUserId from subscribe where fromUserI"
+			+ "d= :principalId) ORDER BY id DESC", nativeQuery=true)
 	Page<Image> mStory(int principalId,Pageable pageable); // 이 mStory를 가져올 때, 3건씩 정렬해서 가져옴 
+	
+	@Query(value="select i.* from image i inner join (select imageId, count(imageId) likeCount from likes group by imageId) c on i.id = c.imageId order by likecount DESC",nativeQuery=true)
+	List<Image> mPopular();
+	
 }

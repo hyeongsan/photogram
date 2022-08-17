f227ca9bdb3f7c970b910fe4a11a649951d823be
hyeongsan <sanstory12rt@naver.com>
2022-08-17 19:08:55 +0900

    댓글쓰기 초기세팅

---
+++ photogram/src/main/java/com/cos/photogramstart/domain/comment/Comment.java
@@ -0,0 +1,51 @@
+package com.cos.photogramstart.domain.comment;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor 
+@NoArgsConstructor 
+@Entity 
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private int id;
+	
+	@Column(length = 100, nullable = false) // 제약조건
+	private String content;
+	
+	@JoinColumn(name="userId") // DB에 만들어질 컬럼명
+	@ManyToOne(fetch = FetchType.EAGER) // ManyToOne은 기본전략 EAGER
+	private User user; // 한명의 유저는 댓글을 여러개
+	
+	@JoinColumn(name="imageId")
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Image image; // 하나의 이미지는 댓글을 여러개
+	
+	private LocalDateTime createDate; // 시간
+	
+	@PrePersist //DB에 insert되기 직전에 실행
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+
+}

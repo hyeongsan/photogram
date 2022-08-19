3da830958371ae6ba8bf5f5e9d244dde3dc862b1
san <sanstory12rt@naver.com>
2022-08-20 02:14:13 +0900

    0820 oauth2_facebook 완료

--- photogram/src/main/java/com/cos/photogramstart/config/auth/PrincipalDetails.java
+++ photogram/src/main/java/com/cos/photogramstart/config/auth/PrincipalDetails.java
@@ -2,26 +2,33 @@ package com.cos.photogramstart.config.auth;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import com.cos.photogramstart.domain.user.User;
 
 import lombok.Data;
 
 @Data
-public class PrincipalDetails implements UserDetails{
+public class PrincipalDetails implements UserDetails, OAuth2User{
 
 
 	private static final long serialVersionUID = 1L;
 	
-	private User user; // 이걸적은이유는 여기(PrincipalDetails.java)에다가 내 User객체를 담고싶어서					
+	private User user; // 이걸적은이유는 여기(PrincipalDetails.java)에다가 내 User객체를 담고싶어서		
+	private Map<String, Object>attributes;
 	
 	public PrincipalDetails(User user) {
 		this.user=user;
 	}
 
+	public PrincipalDetails(User user, Map<String, Object> attributes) {
+		this.user=user;
+	}
+	
 	//권한: 한개가 아닐 수 있음.(3개 이상의 권한)
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() { //권한을 가져오는 함수
@@ -65,6 +72,18 @@ public class PrincipalDetails implements UserDetails{
 	@Override
 	public boolean isEnabled() {
 		return true; // 니 계정이 활성화 되어있니 ? 한 10년동안 로그인안하면 비활성화 되겠죠?
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		// TODO Auto-generated method stub
+		return attributes;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return (String)attributes.get("name"); //오브젝트타입이므로 String으로 다운캐스팅해줘야한다.
 	}
 	
 }

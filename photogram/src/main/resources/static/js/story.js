e0209c2aa2425fad1c84e80104c0766ff27cd60e
san <sanstory12rt@naver.com>
2022-08-19 09:28:09 +0900

    0819 댓글삭제완료

--- photogram/src/main/resources/static/js/story.js
+++ photogram/src/main/resources/static/js/story.js
@@ -6,6 +6,12 @@
 	(4) 댓글쓰기
 	(5) 댓글삭제
  */
+ 
+// (0) 현재 로긴한 사용자 아이디
+let principalId = $("#principalId").val();
+
+
+
 
 // (1) 스토리 로드하기
 let page = 0;
@@ -71,22 +77,29 @@ function getStoryItem(image) {
 		<div id="storyCommentList-${image.id}">`;
 
 
-			image.comments.forEach((comment)=>{
-				item +=`
-				<div class="sl__item__contents__comment" id="storyCommentItem-${comment.id}">
-				<p>
-					<b>${comment.user.username} :</b> ${comment.content}
-				</p>
-
-				<button>
-					<i class="fas fa-times"></i>
-				</button>
-			</div>`;
+	image.comments.forEach((comment)=>{
+		item +=`
+		<div class="sl__item__contents__comment" id="storyCommentItem-${comment.id}">
+		<p>
+			<b>${comment.user.username} :</b> ${comment.content}
+		</p>`;
+				
+				
+	if(principalId == comment.user.id){
+								
+		item += `<button onclick="deleteComment(${comment.id})">
+						<i class="fas fa-times"></i>
+					</button>`;
+					
+					}
+				
+		
+		item += `</div>`;	
+			
+			
 			});
 
-			
-
-
+	
 
 		item += `</div>
 
@@ -207,7 +220,7 @@ function addComment(imageId) {
 		      <b>${comment.user.username} :</b>
 		      ${comment.content}
 		    </p>
-		    <button><i class="fas fa-times"></i></button>
+		    <button onclick="deleteComment(${comment.id})"><i class="fas fa-times"></i></button>
 		  </div>`;
 		  
 	commentList.prepend(content);
@@ -221,8 +234,17 @@ function addComment(imageId) {
 }
 
 // (5) 댓글 삭제
-function deleteComment() {
-
+function deleteComment(commentId) {
+	$.ajax({
+		type:"delete",
+		url:`/api/comment/${commentId}`,
+		dataType:"json"
+	}).done(res=>{
+		console.log("성공",res);
+		$(`#storyCommentItem-${commentId}`).remove();
+	}).fail(error=>{
+		console.log("오류",error);
+	})
 }
 
 
